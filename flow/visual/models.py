a591165f5c4abb7c8463bc5b86d860133d98b0b3
Vojta Tomas <tomas@vojtatom.cz>
2019-05-02 22:18:07 +0200

    Thursday updates

--- flow/visual/models.py
+++ flow/visual/models.py
@@ -15,7 +15,7 @@ from django.core.files.base import ContentFile
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
+    affiliation = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
     objects = models.Manager()
