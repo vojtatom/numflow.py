e17b418881e28a23667b97ad9dce8ce45d7b5b2d
Vojta Tomas <hello@vojtatom.cz>
2024-04-04 20:24:03 +0000

    docs: updating documentation to static version

--- docs-src/vite.config.js
+++ docs-src/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/numflow.py",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+    },
     plugins: [
-        react(),
         ViteImageOptimizer({
             png: {
                 quality: 50,
